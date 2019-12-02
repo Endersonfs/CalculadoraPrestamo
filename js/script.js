1ae8ee5d66b1102f94746c953a9fcb06b3a394ea
Enderson Florian Solano <efloriansolano@gmail.com>
2019-12-02 05:32:09 +0100

    Agregando precio total con los meses a pagar

--- js/script.js
+++ js/script.js
@@ -7,51 +7,53 @@ var montoprestamos=0;
 var tiempomes =0;
 var resultado=0;
 montoprestamos = document.getElementById("montoprestamos").value;
-tiempomes = document.getElementById("tiempomes").value; 
-//validacion 
-if(montoprestamos>=1000 &&  montoprestamos < 20000)
+tiempomes = document.getElementById("tiempomes").value;
+if(tiempomes ==="" || montoprestamos==="")
 {
-    resultado = montoprestamos * 0.12;
-    alert("Cantidad a un 12% : "+resultado);
+    alert("Debes llenar todos los campos");
+}else
+{    
+//validacion 
+if(montoprestamos>=1000 &&  montoprestamos < 5999)
+{
+    resultado = montoprestamos * 0.15;
+    alert("Cantidad a un 15% y el precio a pagar mensual es: :"+((parseFloat(resultado)+parseFloat(montoprestamos))/tiempomes));
     montoprestamos =0;
 
-}else if(montoprestamos >=21000 && montoprestamos < 40000){
+}else if(montoprestamos >=5000 && montoprestamos < 11999){
+    resultado = montoprestamos * 0.13;
+    alert("Cantidad a un 13% y el precio a pagar mensual es: :"+((parseFloat(resultado)+parseFloat(montoprestamos))/tiempomes));
+    montoprestamos =0;
+}
+else if(montoprestamos >=12000 && montoprestamos < 20999){
+    resultado = montoprestamos * 0.12;
+    alert("Cantidad a un 12% y el precio a pagar mensual es: :"+((parseFloat(resultado)+parseFloat(montoprestamos))/tiempomes));
+    montoprestamos =0;
+}
+else if(montoprestamos >=21000 && montoprestamos < 40999){
     resultado = montoprestamos * 0.10;
-    alert("Cantidad a un 10% : "+resultado);
+    alert("Cantidad a un 10% y el precio a pagar mensual es: :"+((parseFloat(resultado)+parseFloat(montoprestamos))/tiempomes));
     montoprestamos =0;
 }
-else if(montoprestamos >=41000 && montoprestamos < 80000){
+else if(montoprestamos >=41000 && montoprestamos < 90999){
     resultado = montoprestamos * 0.8;
-    alert("Cantidad a un 8% : "+resultado);
+    alert("Cantidad a un 8% y el precio a pagar mensual es: :"+((parseFloat(resultado)+parseFloat(montoprestamos))/tiempomes));
     montoprestamos =0;
 }
-else if(montoprestamos >=81000 && montoprestamos < 120000){
+else if(montoprestamos >=91000 && montoprestamos < 160999){
     resultado = montoprestamos * 0.7;
-    alert("Cantidad a un 7% : "+resultado);
+    alert("Cantidad a un 7% y el precio a pagar mensual es: :"+((parseFloat(resultado)+parseFloat(montoprestamos))/tiempomes));
     montoprestamos =0;
 }
-else if(montoprestamos >=121000 && montoprestamos < 160000){
+else if(montoprestamos >=161999){
     resultado = montoprestamos * 0.6;
-    alert("Cantidad a un 6% : "+resultado);
-    montoprestamos =0;
-}
-else if(montoprestamos >=161000 && montoprestamos < 200000){
-    resultado = montoprestamos * 0.605;
-    alert("Cantidad a un 6.5% : "+resultado);
-    montoprestamos =0;
-}
-else if(montoprestamos >201000 && montoprestamos <250000){
-    resultado = montoprestamos * 0.5;
-    alert("Cantidad a un 5% : "+resultado);
-    montoprestamos =0;
-}
-else if(montoprestamos >=251000){
-    resultado = montoprestamos * 0.5050;
-    alert("Cantidad a un 5.50% : "+resultado);
+    alert("Cantidad a un 6% y el precio a pagar mensual es: :"+((parseFloat(resultado)+parseFloat(montoprestamos))/tiempomes));
     montoprestamos =0;
 }
 else {
     alert("Esta fuera del rango");
+}
+
 }        
 // mostrando resultado
 resultado = document.getElementById("compuesto");
